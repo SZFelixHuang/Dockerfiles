fa91192fc10970959658d977ce31f6010fe496fe
root <root@openshift-installer.felix.com>
2020-11-09 22:06:11 +0800

    update s2i scripts

--- s2i-builder-image/install.sh
+++ s2i-builder-image/install.sh
@@ -1,3 +1,3 @@
 make
-docker tag s2i-build-image:latest registry.felix.com/devops/s2i-builder-image:latest
+docker tag s2i-builder-image:latest registry.felix.com/devops/s2i-builder-image:latest
 docker push  registry.felix.com/devops/s2i-builder-image:latest
