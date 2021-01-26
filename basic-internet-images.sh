d77da0d90bc6cf3a7e770cbe5ee6915d4ff6faf5
Felix <Felix_Huang@outlook.com>
2021-01-26 22:48:27 +0800

    add mysql and redis images

--- basic-internet-images.sh
+++ basic-internet-images.sh
@@ -5,3 +5,15 @@ docker pull centos:centos8
 docker tag centos:centos8 registry.felix.com/devops/centos:8
 
 docker push registry.felix.com/devops/centos:8
+
+docker pull mysql:latest
+
+docker tag mysql:latest registry.felix.com/devops/mysql:latest
+
+docker push registry.felix.com/devops/mysql:latest
+
+docker pull redis:latest
+
+docker tag redis:latest registry.felix.com/devops/redis:latest
+
+docker push registry.felix.com/devops/redis:latest
