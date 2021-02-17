9918762f3f7e9b62f6f309c6d7d4cc14f8257cca
Felix <Felix_Huang@outlook.com>
2021-02-17 14:20:55 +0800

    add seata server image

--- basic-internet-images.sh
+++ basic-internet-images.sh
@@ -14,6 +14,12 @@ docker push registry.felix.com/devops/redis:latest
 
 docker pull registry.redhat.io/openshift3/ose-haproxy-router:latest
 
-docker tag registry.felix.com/openshift/ose-haproxy-router:v3.11
+docker tag registry.redhat.io/openshift3/ose-haproxy-router:latest registry.felix.com/openshift/ose-haproxy-router:v3.11
 
 docker push registry.felix.com/openshift/ose-haproxy-router:v3.11
+
+docker pull seataio/seata-server:latest
+
+docker tag seataio/seata-server:latest registry.felix.com/seata/seata-server:latest
+
+docker push registry.felix.com/seata/seata-server:latest
